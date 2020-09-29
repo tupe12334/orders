cf631d27975dd0051f152e115f33696330b04c7c
Ofek <tupe12334@gmail.com>
2020-09-29 17:42:53 +0300

    Updates

---
+++ precache-manifest.9b04e13062b2c33ca0cbc9185e6ff817.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea160c5b2b66cb808a021888b66f6920",
+    "url": "/orders/.%20/index.html"
+  },
+  {
+    "revision": "dafd1e678f098697bfb6",
+    "url": "/orders/.%20/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c4d650cd68e2c71234b4",
+    "url": "/orders/.%20/static/js/2.725aec9e.chunk.js"
+  },
+  {
+    "revision": "51f46ad09d5db8752dc95f27e9e186dd",
+    "url": "/orders/.%20/static/js/2.725aec9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dafd1e678f098697bfb6",
+    "url": "/orders/.%20/static/js/main.d6360f10.chunk.js"
+  },
+  {
+    "revision": "c01b25c795a807c13be4",
+    "url": "/orders/.%20/static/js/runtime-main.17719b44.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/orders/.%20/static/media/logo.5d5d9eef.svg"
+  }
+]);
